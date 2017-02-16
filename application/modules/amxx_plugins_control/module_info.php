0347b621e6d27456b1ceb9d95401a8bcdb3a9075
ET-Nik <nikita.hldm@gmail.com>
2017-02-16 23:35:40 +0300

    Update version [GameAP 2.0]

--- application/modules/amxx_plugins_control/module_info.php
+++ application/modules/amxx_plugins_control/module_info.php
@@ -3,11 +3,11 @@
 $module_info['name'] 			= 'Amxx plugins control';													// Имя модуля
 $module_info['cron_script'] 	= '';	
 $module_info['description'] 	= 'Модуль для удобной установки, включения и выключения плагинов AMX (только для GoldSource серверов)';		// Описание
-$module_info['version'] 		= '1.0';																// Версия модуля
+$module_info['version'] 		= '2.0';																// Версия модуля
 $module_info['show_in_menu'] 	= 1;																	// Не показывать в меню
 $module_info['access'] 			= 'users';
 $module_info['developer'] 		= 'ET-NiK';
 $module_info['site'] 			= 'http://hldm.org';
 $module_info['email'] 			= 'nikita@hldm.org';
-$module_info['copyright'] 		= '(c) 2013, ET-NiK (http://hldm.org)';
+$module_info['copyright'] 		= '(c) 2013-2016, ET-NiK (http://hldm.org)';
 $module_info['license'] 		= 'http://www.gameap.ru/license.html';
